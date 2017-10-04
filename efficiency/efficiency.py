93fa02598bf73bde1ee2f8df615f03311178c951
erramuzpe <asier.erramuzpe@gmail.com>
2017-10-04 11:48:44 +0200

    what to do

---
+++ efficiency/efficiency.py
@@ -0,0 +1,45 @@
+# -*- coding: utf-8 -*-
+"""
+Module to work on Ane's problem of efficiency in Tractography streamlines
+"""
+
+"""
+1: Get Corpus Callosum mask and extract the upper center part.
+ie: fix [y] and [z] (upper points in median sagittal plane)
+This will be the ground truth mask
+"""
+
+
+
+
+
+
+
+
+
+
+"""
+2: Transform the CC mask to subjects DWI space
+"""
+
+
+
+
+
+
+
+
+
+
+
+"""
+3: Calculate the most efficient path between 2 given points in the space 
+crossing CC mask
+"""
+
+
+
+
+
+
+
